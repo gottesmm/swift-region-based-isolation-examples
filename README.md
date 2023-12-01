38109ee7f5b20b5e631aaaf7b59e45ab66de10e9
Michael Gottesman <mgottesman@apple.com>
2023-12-01 15:43:26 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RegionIsolationExamples
+
+A description of this package.
---
+++ Sources/RegionIsolationExamples/RegionIsolationExamples.swift
@@ -0,0 +1,6 @@
+public struct RegionIsolationExamples {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
